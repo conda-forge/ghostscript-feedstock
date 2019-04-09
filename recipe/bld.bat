8a807365100305297dcc356d4f34d5035607d540
Leonardo Uieda <leouieda@gmail.com>
2019-04-08 15:12:51 -1000

    Try building in a separate directory

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,9 @@
-nmake -f psi\msvc.mak GSROOTDIR="%LIBRARY_PREFIX%" WIN64= MSVC_VERSION=%VS_MAJOR:"=% DEVSTUDIO=
+mkdir build
 
+nmake -f psi\msvc.mak GSROOTDIR="build" WIN64= MSVC_VERSION=%VS_MAJOR:"=% DEVSTUDIO=
 if errorlevel 1 exit 1
+
+robocopy build\ %LIBRARY_PREFIX% *.* /E
+if %ERRORLEVEL% GTR 3 exit 1
 
 exit 0
