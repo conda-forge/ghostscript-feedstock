85d8a1a5d7612b08675ec008df867f8467e21f78
Leonardo Uieda <leouieda@gmail.com>
2019-06-19 14:36:32 -1000

    Just let conda set the dev studio paths

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-nmake -f psi\msvc.mak GSROOTDIR=%LIBRARY_PREFIX% WIN64= MSVC_VERSION=%VS_MAJOR%
+nmake -f psi\msvc.mak GSROOTDIR=%LIBRARY_PREFIX% WIN64= MSVC_VERSION=%VS_MAJOR% DEVSTUDIO=
 if errorlevel 1 exit 1
 
 exit 0
