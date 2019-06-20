8a95b2497d457cfe943eafdcd774ffb42c0a7a9d
Leonardo Uieda <leouieda@gmail.com>
2019-06-19 14:24:06 -1000

    Revert back to a plain nmake call

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,4 @@
-set LIB=%LIBRARY_LIB%;%LIB%
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%
-
-mkdir build
-
-nmake -f psi\msvc.mak GSROOTDIR="build" WIN64= MSVC_VERSION=%VS_MAJOR:"=% DEVSTUDIO=
+nmake -f psi\msvc.mak GSROOTDIR=%LIBRARY_PREFIX% WIN64= MSVC_VERSION=%VS_MAJOR%
 if errorlevel 1 exit 1
-
-robocopy build\ %LIBRARY_PREFIX% *.* /E
-if %ERRORLEVEL% GTR 3 exit 1
 
 exit 0
