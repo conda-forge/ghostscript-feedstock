46b6d60804a9371bed01988d30b7e06905e92142
Filipe Fernandes <ocefpaf@gmail.com>
2019-07-07 09:12:59 -0500

    @gillins suggestions

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,10 @@
 nmake -f psi\msvc.mak GSROOTDIR=%LIBRARY_PREFIX% WIN64= MSVC_VERSION=%VS_MAJOR% DEVSTUDIO=
 if errorlevel 1 exit 1
 
-exit 0
+move bin\*.dll %LIBRARY_BIN%
+cp bin\gswin64c.exe %LIBRARY_BIN%\gs.exe
+move bin\*.exe %LIBRARY_BIN%
+move bin\*.lib %LIBRARY_LIB%
+move bin\*.exp %LIBRARY_LIB%
+move Resource %LIBRARY_PREFIX%\share
+REM fonts
