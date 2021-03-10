02fa0661f20601830d93e7a6aab14df2c5300fda
Felix Mölder <felix.moelder@uni-due.de>
2021-03-10 12:40:56 +0100

    Add ldflags

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [[ $(uname) == Darwin ]]; then
+  export CC=clang
+  export CXX=clang++
+  export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -headerpad_max_install_names $LDFLAGS"
+  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+  export MACOSX_DEPLOYMENT_TARGET="10.9"
+  export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
+fi
+
 ./configure --prefix=${PREFIX}
 
 make -j${CPU_COUNT}
