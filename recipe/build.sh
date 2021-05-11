837b2ca10f64bdc369a25b97b6b0d9a21d396c2d
Marcel Bargull <marcel.bargull@udo.edu>
2021-05-11 10:31:57 +0200

    Build without Tesseract OCR support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-./configure --prefix=${PREFIX}
+# Build without Tesseract OCR support:
+#   - Sources for 9.53 did not include everything for default Tesseract support.
+#     => conda-forge::ghostscript<9.54 does not have Tesseract support.
+#   - Upstream does not support linking to external Tesseract library.
+#     - macOS builds fail because ./configure sets -stdlib=stdlibc++.
+#     - We try to avoid vendoring at conda-forge.
+#       => Better to wait for upstream to support linking to external library.
+./configure \
+    --prefix=${PREFIX} \
+    --without-tesseract
 
 make -j${CPU_COUNT}
 make so -j${CPU_COUNT}
